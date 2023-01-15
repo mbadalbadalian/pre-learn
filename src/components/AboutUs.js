74c3c85331b5d933c27915b3a136c82db525b72e
Sanjula Ganepola <sanjulagane@gmail.com>
2023-01-15 08:35:23 -0500

    update about us

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -60,7 +60,7 @@ function AboutUs({ setCurrentPageTab }) {
                         <h2>
                             Sanjula Ganepola
                         </h2>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        I am currently a fourth-year student at McMaster University studying Software Engineering and Management. I have experience using Java, Python, C, SQL, and I have explored areas including object-oriented design, data structures, database design, software requirements, security, and testing.
                     </div>
                 </div>
             </div>
