75680ee382e0a83770e0e0c0c17bb2d1b0f53bda
tsuiglen <glentsui@yahoo.com>
2023-01-15 06:30:40 -0500

    Push formating changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,8 @@
 import '../styles/Home.css';
 import * as constants from "../utils/constants";
+import robot from "../assets/robot.png"
+import computer from "../assets/computer.png"
+import chart from "../assets/chart.png"
 
 function Home({ setCurrentPageTab, setCurrentCourse }) {
   return (
@@ -16,16 +19,39 @@ function Home({ setCurrentPageTab, setCurrentCourse }) {
         <h2>
           Enter your course code below!
         </h2>
-        <form action="" class="search-bar">
-          <input onChange={(e) => { setCurrentCourse(e.target.value) }} name="search" pattern=".*\S.*" required></input>
-          <button class="search-btn" type="submit" onClick={() => {
-          setCurrentPageTab(constants.PAGES.QUESTIONS);}}>
-            <span>Search</span>
-          </button>
-        </form>
+        <div class="searchBarBtn">
+          <form action="" class="search-bar">
+            <input onChange={(e) => { setCurrentCourse(e.target.value) }} name="search" pattern=".*\S.*" required></input>
+            <button class="search-btn" type="submit" onClick={() => {
+            setCurrentPageTab(constants.PAGES.QUESTIONS);}}>
+              <span>Search</span>
+            </button>
+          </form>
+        </div>
+        
       </div>
       <div class="techs">
-
+            <div class="column">
+              <img src={computer} alt = "Computer Icon" width = "100"></img>
+              <h2>
+                Fetch relevant concepts from course directory
+              </h2>
+              Using the course code from user input, PreLearn fetches and extracts the course data to find relevant fundamental concepts.
+            </div>
+            <div class="column">
+              <img src={robot} alt = "Robot Icon" width = "100"></img>
+              <h2>
+                ML sorts the learning videos by most positively reviewed
+              </h2>
+              Trained on Sentiment Analysis, the algorithm uses the Gaussian naive base model to sort the videos by the most positive comments for each concept.
+            </div>
+            <div class="column">
+              <img src={chart} alt = "Chart Icon" width = "100"></img>
+              <h2>
+                Displays a score based on their understanding
+              </h2>
+              With the final score result, the user can determine their readiness for the course, and be presented with the best learning videos for concepts requiring further improvement.
+            </div>
         
       </div>
     </div>
